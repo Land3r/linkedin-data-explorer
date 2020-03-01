1d7490a02569d1810f639d010b066cb0799edfe2
Nicolas Gordat <nicolas.gordat@gmail.com>
2020-03-01 21:38:51 +0100

    Initial commit

---
+++ src/pages/Upload.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class=''>
+    <FileUploadArea></FileUploadArea>
+  </q-page>
+</template>
+
+<script>
+import FileUploadArea from '../components/FileUploadArea'
+
+export default {
+  name: 'PageIndex',
+  components: {
+    FileUploadArea: FileUploadArea
+  }
+}
+</script>
