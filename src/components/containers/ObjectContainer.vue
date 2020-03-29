7afa26445dde42b51847b3b8617363357c0a0ce3
Nicolas Gordat <nicolas.gordat@gmail.com>
2020-03-29 05:05:38 +0200

    Json Object Display

---
+++ src/components/containers/ObjectContainer.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="q-pa-md">
+    <div class="text-h6 object-container-title">{{title}}</div>
+    <div ref="objectContainer" style="white-space:wrap !important">
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.object-container-title {
+  text-align: center
+}
+
+// Overrides the words non wrapping on the next line
+.json-formatter-row .json-formatter-string {
+  white-space: normal
+}
+</style>
+
+<script>
+import JSONFormatter from 'json-formatter-js'
+
+export default {
+  name: 'ObjectContainer',
+  props: {
+    title: {
+      type: String,
+      required: false
+    },
+    data: {
+      type: [Object, Array],
+      required: true
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  computed: {
+    formattedJson () {
+      return new JSONFormatter(this.data).render()
+    }
+  },
+  mounted () {
+    this.$refs.objectContainer.appendChild(this.formattedJson)
+  }
+}
+</script>
